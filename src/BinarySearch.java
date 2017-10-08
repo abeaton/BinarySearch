29625969314931ebcfbd9e6c2886846c9fe544f8
Adam Beaton <abeaton@Adams-MacBook-Pro.local>
2017-10-08 16:28:36 -0400

    Implemented binary search recursively

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,6 +1,54 @@
+import java.lang.reflect.MalformedParametersException;
+
 public class BinarySearch {
-    public static <T extends Comparable> int Find(T element, T[] array){
-        return -1;
+    public static final int NonExistantIndex = -1;
+
+    /**
+     * A recursive implementation of the binary search algorithm
+     * @param elementToFind => The element in the array to find the index of
+     * @param array => a non-empty, sorted array that is being searched
+     * @param <T> => Comparable, which ensures an implementation of compareTo
+     * @return => the index that elementToFind is located at in array, otherwise -1
+     */
+    public static <T extends Comparable> int Find(T elementToFind, T[] array){
+        if(array == null || array.length == 0){
+            throw new MalformedParametersException();
+        }
+
+        return Find(elementToFind, array, 0, array.length -1);
+    }
+
+    private static <T extends Comparable> int Find(T elementToFind, T[] array, int firstIndex, int lastIndex){
+        if(firstIndex > lastIndex){
+            throw new MalformedParametersException("This should never be hit as this is a private function");
+        }
+
+        int currentIndex = (firstIndex + lastIndex) / 2;
+        T currentElement = array[currentIndex];
+
+        if(AreEqual(elementToFind, currentElement))
+        {
+            return currentIndex;
+        }
+        else
+        {
+            if(firstIndex == lastIndex)
+            {
+                return NonExistantIndex;
+            }
+            else
+            {
+                int comparison = elementToFind.compareTo(currentElement);
+                if(comparison < 0)
+                {
+                    return Find(elementToFind, array, firstIndex, currentIndex);
+                }
+                else
+                {
+                    return Find(elementToFind, array, currentIndex+1, lastIndex);
+                }
+            }
+        }
     }
 
     /**
--- src/BinarySearchTest.java
+++ src/BinarySearchTest.java
@@ -1,7 +1,125 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.MalformedParametersException;
+
 public class BinarySearchTest {
+    @Test (expected = MalformedParametersException.class)
+    public void TestFind_NullArray(){
+        BinarySearch.Find(2, null);
+    }
+
+    @Test (expected = MalformedParametersException.class)
+    public void TestFind_EmptyArray(){
+        BinarySearch.Find(2, new Integer[0]);
+    }
+
+    @Test
+    public void TestFind_Exists_ArrayLengthOne(){
+        Double[] array = { 2.5 };
+        int index = BinarySearch.Find(2.5, array);
+        Assert.assertEquals(0, index);
+    }
+
+    @Test
+    public void TestFind_NotExists_ArrayLengthOne(){
+        Double[] array = { 2.5 };
+        int index = BinarySearch.Find(2.51, array);
+        Assert.assertEquals(BinarySearch.NonExistantIndex, index);
+    }
+
+    @Test
+    public void TestFind_Exists_ArrayLengthTwo_Index0(){
+        Double[] array = { 2.5, 4.3 };
+
+        int index = BinarySearch.Find(2.5, array);
+        Assert.assertEquals(0, index);
+    }
+
+    @Test
+    public void TestFind_Exists_ArrayLengthTwo_Index1(){
+        String[] array = { "a", "z" };
+
+        int index = BinarySearch.Find("z", array);
+        Assert.assertEquals(1, index);
+    }
+
+    @Test
+    public void TestFind_NotExists_ArrayLengthTwo(){
+        Double[] array = { 2.5, 4.3 };
+        int index = BinarySearch.Find(1.1, array);
+        Assert.assertEquals(BinarySearch.NonExistantIndex, index);
+    }
+
+    @Test
+    public void TestFind_ArrayLength3(){
+        Integer[] array = { 1, 2, 3 };
+
+        int index0 = BinarySearch.Find(array[0], array);
+        Assert.assertEquals(0, index0);
+
+        int index1 = BinarySearch.Find(array[1], array);
+        Assert.assertEquals(1, index1);
+
+        int index2 = BinarySearch.Find(array[2], array);
+        Assert.assertEquals(2, index2);
+
+        int indexNotExists = BinarySearch.Find(50, array);
+        Assert.assertEquals(BinarySearch.NonExistantIndex, indexNotExists);
+    }
+
+    @Test
+    public void TestFind_ArrayLength4(){
+        Integer[] array = { 1, 2, 3, 4 };
+
+        int index0 = BinarySearch.Find(array[0], array);
+        Assert.assertEquals(0, index0);
+
+        int index1 = BinarySearch.Find(array[1], array);
+        Assert.assertEquals(1, index1);
+
+        int index2 = BinarySearch.Find(array[2], array);
+        Assert.assertEquals(2, index2);
+
+        int index3 = BinarySearch.Find(array[3], array);
+        Assert.assertEquals(3, index3);
+
+        int indexNotExists = BinarySearch.Find(50, array);
+        Assert.assertEquals(BinarySearch.NonExistantIndex, indexNotExists);
+    }
+
+    @Test
+    public void TestFind_ArrayLength5(){
+        Integer[] array = { 1, 2, 3, 4, 5 };
+
+        int index0 = BinarySearch.Find(array[0], array);
+        Assert.assertEquals(0, index0);
+
+        int index1 = BinarySearch.Find(array[1], array);
+        Assert.assertEquals(1, index1);
+
+        int index2 = BinarySearch.Find(array[2], array);
+        Assert.assertEquals(2, index2);
+
+        int index3 = BinarySearch.Find(array[3], array);
+        Assert.assertEquals(3, index3);
+
+        int index4 = BinarySearch.Find(array[4], array);
+        Assert.assertEquals(4, index4);
+
+        int indexNotExists = BinarySearch.Find(50, array);
+        Assert.assertEquals(BinarySearch.NonExistantIndex, indexNotExists);
+    }
+
+    @Test
+    public void TestFind_Long(){
+        int elementToFind = 23;
+        Integer[] array = { -5, -3, -1, 3, 6, 7, 19, 21, elementToFind, 26, 27, 30, 39, 40 };
+
+        int index = BinarySearch.Find(elementToFind, array);
+        Assert.assertEquals(8, index);
+    }
+
     @Test
     public void TestAreEqual_Equal(){
         Integer element1 = 50;
